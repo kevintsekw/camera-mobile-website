4d77b1bf92ab565a3a6ee62b9b99e77b27bd3c36
kevintsekw <78713026+kevintsekw@users.noreply.github.com>
2021-02-08 18:30:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+//import { FileSaver } from './FileSaver.js';
+/*
+var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+    FileSaver.saveAs(blob, "world.txt");
+*/
+
+
 var frontCamera = false;
 var track = null;
 var currentStream;
@@ -23,7 +30,8 @@ function cameraStart() {
         });
     }
 
-    var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
+//    var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
+    var constraints = { video: { facingMode: ("environment") }, audio: false };
 
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -36,13 +44,38 @@ function cameraStart() {
         });
 }
 
+
+
 // Take a photo when takePhotoButton is clicked
 takePhotoButton.onclick = function() {
     cameraDevice.width = cameraView.videoWidth;
     cameraDevice.height = cameraView.videoHeight;
     cameraDevice.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraDisplay.src = cameraDevice.toDataURL("image/webp");
+    cameraDisplay.src = cameraDevice.toDataURL("image/png");
+//    cameraDisplay.src = cameraDevice.toDataURL("image/webp");
+    
+      // here is the most important part because if you dont replace you will get a DOM 18 exception.
+//var image = cameraDevice.toDataURL("image/webp").replace("image/webp", "image/octet-stream");
+//cameraDisplay.src(cameraDevice.toDataURL("image/webp").replace("image/webp", "image/octet-stream"));
+
+
+        var link = document.createElement('a');
+        link.download = 'test.png';
+        link.href = cameraDevice.toDataURL("image/png");
+        link.style.display = "none";
+        link.click();
+
+/*
+    dataurl = cameraDisplay.src = cameraDevice.toDataURL("image/png");
+canvas2image.saveAsPNG(cameraDevice);
+*/  
+/*
+cameraDevice.toBlob(function(blob) {
+      FileSaver.saveAs(blob, 'mypng.png');
+    });
+*/
     cameraDisplay.classList.add("photo-taken");
+
 };
 
 frontCameraButton.onclick = function() { 
@@ -60,3 +93,4 @@ frontCameraButton.onclick = function() {
 // 1st parameter: Event type
 // 2nd parameter:Function to be called when the event occurs
 window.addEventListener("load", cameraStart);
+
