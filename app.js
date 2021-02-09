68561416f003c6235bdd319eacb60e623c987554
kevintsekw <78713026+kevintsekw@users.noreply.github.com>
2021-02-09 09:23:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var currentStream;
 const
     cameraView = document.querySelector("#camera-view"),
     cameraDevice = document.querySelector("#camera-device"),
-    cameraDisplay = document.querySelector("#photo-display"),
+    photoDisplay = document.querySelector("#photo-display"),
     takePhotoButton = document.querySelector("#take-photo-button");
     frontCameraButton = document.querySelector("#front-camera-button");
 
@@ -25,16 +25,48 @@ function cameraStart() {
 
 //    var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
     var constraints = { video: { facingMode: ("environment") }, audio: false };
-
+    
+/*
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
             currentStream = stream;
-            cameraView.srcObject = stream;
+            
+            const track = stream.getVideoTracks()[0];
+
+            imageCapture = new ImageCapture(track);
+            
+            imageCapture.takePhoto()
+            .then(blob => createImageBitmap(blob))
+            .then(imageBitmap => {
+                drawCanvas(photoDisplay, imageBitmap);
+            });
+   
         })
         .catch(function(error) {
             console.error("Error happened.", error);
         });
+*/
+navigator.mediaDevices.getUserMedia({video: true})
+  .then(mediaStream => {
+    document.querySelector('video').srcObject = mediaStream;
+
+      videoDevice = mediaStream.getVideoTracks()[0];
+  // Check if this device supports a picture mode...
+  let captureDevice = new ImageCapture(videoDevice);
+  if (captureDevice) {
+    captureDevice.grabFrame().then(processFrame).catch();
+  }
+
+    
+    
+    })
+  .catch();
+}
+function processFrame(imageBitmap) {
+  photoDisplay.width = imageBitmap.width;
+  photoDisplay.height = imageBitmap.height;
+  photoDisplay.getContext('2d').drawImage(imageBitmap, 0, 0);
 }
 
 
@@ -44,22 +76,29 @@ takePhotoButton.onclick = function() {
     cameraDevice.width = cameraView.videoWidth;
     cameraDevice.height = cameraView.videoHeight;
     cameraDevice.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraDisplay.src = cameraDevice.toDataURL("image/png");
-//    cameraDisplay.src = cameraDevice.toDataURL("image/webp");
+    photoDisplay.src = cameraDevice.toDataURL("image/png");
+//    photoDisplay.src = cameraDevice.toDataURL("image/webp");
     
       // here is the most important part because if you dont replace you will get a DOM 18 exception.
 //var image = cameraDevice.toDataURL("image/webp").replace("image/webp", "image/octet-stream");
-//cameraDisplay.src(cameraDevice.toDataURL("image/webp").replace("image/webp", "image/octet-stream"));
+//photoDisplay.src(cameraDevice.toDataURL("image/webp").replace("image/webp", "image/octet-stream"));
 
 
+
+
+/*
         var link = document.createElement('a');
         link.download = 'test.png';
         link.href = cameraDevice.toDataURL("image/png");
         link.style.display = "none";
         link.click();
+*/
+
+
+
 
 /*
-    dataurl = cameraDisplay.src = cameraDevice.toDataURL("image/png");
+    dataurl = photoDisplay.src = cameraDevice.toDataURL("image/png");
 canvas2image.saveAsPNG(cameraDevice);
 */  
 /*
@@ -67,7 +106,7 @@ cameraDevice.toBlob(function(blob) {
       FileSaver.saveAs(blob, 'mypng.png');
     });
 */
-    cameraDisplay.classList.add("photo-taken");
+    photoDisplay.classList.add("photo-taken");
 
 };
 
